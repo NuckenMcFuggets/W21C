05bf5d98d14f813a0aa5e1b61c1f0efde848dc7c
NuckenMcFuggets <cameron.ian.ronald.ord@Outlook.com>
2023-06-11 19:06:40 -0600

    added queries and DB backup

---
+++ queries.sql
@@ -0,0 +1,4 @@
+call create_user('Cameron', 'password');
+call login_proc('Cameron', 'password', 'this is totally a token');
+call create_user('Cameron', 'password');
+call login_proc('Cameron', 'password', 'this is totally a token');
