a932a2934b95c6d1dfa393949f6df6804f9fefb8
NuckenMcFuggets <cameron.ian.ronald.ord@Outlook.com>
2023-06-12 17:26:00 -0600

    added client and login apis

--- python/app.py
+++ python/app.py
@@ -1,26 +1,84 @@
 #importing
 import json
-from flask import Flask
+from flask import Flask, request, make_response, jsonify
 import dbhelper
+import api_helper
+import uuid
 
 app = Flask(__name__)
 
-@app.post('/api/create_user')
+@app.post('/api/client')
 def create_user():
-try:
-      #calls the function in api_helper to loop through the information sent
-      error=api_helper.check_endpoint_info(request.json, ['']) 
-      if(error !=None):
-         return 'something went wrong'
-      #calls the proceedure to insert sent information into the DB
-      results = dbhelper.run_proceedure('CALL create_user(?)', [request.json.get('')])
-      #returns results from db run_proceedure
-      return json.dumps(results, default=str)
-   #error catching
+   try:
+         #calls the function in api_helper to loop through the information sent
+         error=api_helper.check_endpoint_info(request.json, ['username', 'password']) 
+         if(error !=None):
+            return 'something went wrong'
+         #calls the proceedure to insert sent information into the DB
+         results = dbhelper.run_proceedure('CALL create_user(?, ?)', [request.json.get('username'), request.json.get('password')])
+         #returns results from db run_proceedure
+         if(type(results) == list):
+            return make_response(jsonify(results), 200)
+         else:
+            return make_response(jsonify('something how gone wrong'), 500)
+      #error catching
    except TypeError:
       print('Invalid entry, try again')
    except ValueError:
       print('Value outside range, try again')
+      
+      
+@app.post('/api/login')
+def login():
+   try:
+         #calls the function in api_helper to loop through the information sent
+         error=api_helper.check_endpoint_info(request.json, ['username', 'password']) 
+         if(error !=None):
+            return 'something went wrong'
+         token = uuid.uuid4().hex
+         #calls the proceedure to insert sent information into the DB
+         results = dbhelper.run_proceedure('CALL login_proc(?, ?, ?)', [request.json.get('username'), request.json.get('password'), token])
+         #returns results from db run_proceedure
+         if(type(results) == list and results != []):
+            return make_response(jsonify(results), 200)
+         else:
+            return make_response(jsonify(results), 500)
+   #error catching
+   
+   except TypeError:
+      print('Invalid entry, try again')
+   except ValueError:
+      print('Value outside range, try again')
+      
+      
+      
+      
+@app.post('/api/post') 
+def post():
+   try:
+         #calls the function in api_helper to loop through the information sent
+         error=api_helper.check_endpoint_info(request.json, ['token', 'content']) 
+         if(error !=None):
+            return 'something went wrong'
+         #calls the proceedure to insert sent information into the DB
+         results = dbhelper.run_proceedure('CALL create_post(?, ?)', [request.json.get('token'), request.json.get('content')])
+         #returns results from db run_proceedure
+         if(type(results) == list and results != []):
+            return make_response(jsonify(results), 200)
+         else:
+            return make_response(jsonify(results), 500)
+   #error catching
 
+   except TypeError:
+      print('Invalid entry, try again')
+   except ValueError:
+      print('Value outside range, try again')
+      
+         
+         
+            
+         
+
+         
 #running @app
 app.run(debug=True)
--- python/dbhelper.py
+++ python/dbhelper.py
@@ -1,5 +1,14 @@
 import mariadb
 import dbcreds
+#makes returned data look nicer
+
+def convert_data(cursor, results):
+    column_names = [i[0] for i in cursor.description]
+    new_results = []
+    for row in results:
+        new_results.append(dict(zip(column_names, row)))
+    return new_results
+
 #function that gets called on each request using args based on which request was called
 def run_proceedure(sql, args):
     try:
@@ -9,6 +18,7 @@ def run_proceedure(sql, args):
         cursor = conn.cursor()
         cursor.execute(sql, args)
         results = cursor.fetchall()
+        results = convert_data(cursor, results)
         #catching errors and diagnosing them
     except mariadb.ProgrammingError as error:
         print('There is an issue with the DB code: ', error)
